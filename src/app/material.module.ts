94efd97fb387da8e56f0e1b8aff833116c013333
junwoo45 <sinyoorang@gmail.com>
2020-01-28 18:36:35 +0900

    사이드바 수정

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatSidenavModule, MatToolbarModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
-    imports: [ MatButtonModule, MatFormFieldModule, MatInputModule, MatSidenavModule, MatToolbarModule, MatIconModule ],
-    exports: [ MatButtonModule, MatFormFieldModule, MatInputModule, MatSidenavModule, MatToolbarModule, MatIconModule ]
+    imports: [ MatButtonModule, MatFormFieldModule, MatInputModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule ],
+    exports: [ MatButtonModule, MatFormFieldModule, MatInputModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule ]
 })
 export class MaterialModule {}
