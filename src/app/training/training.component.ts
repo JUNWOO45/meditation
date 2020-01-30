f5acbb4ab1c62f5024d405d7147d0769454ceead
junwoo45 <sinyoorang@gmail.com>
2020-01-30 16:22:46 +0900

    파도소리와 빗소리 영상&소리 토글

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./training.component.less']
 })
 export class TrainingComponent implements OnInit {
+  currentVideo = '../../assets/beach.mp4';
+  currentAudio = '../../assets/beach.mp3';
 
+  rainVideo = '../../assets/rain.mp4';
+  beachVideo = '../../assets/beach.mp4';
+  
+  rainAudio = '../../assets/rain.mp3';
+  beachAudio = '../../assets/beach.mp3';
   constructor() { }
 
   ngOnInit() {
   }
 
+  rain(audio) {
+    console.log(audio);
+    this.currentVideo = this.rainVideo;
+    this.currentAudio = this.rainAudio;
+    audio.load();
+    audio.play();
+  }
+
+  beach(audio) {
+    this.currentVideo = this.beachVideo;
+    this.currentAudio = this.beachAudio;
+    audio.load();
+    audio.play();
+  }
 }
