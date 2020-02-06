cdea2de135eaa079fb9af19fce28ee880075aee5
junwoo45 <sinyoorang@gmail.com>
2020-02-06 23:59:39 +0900

    feat: 사용자 편의를 위해 회원가입, 로그인 기능 삭제

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,15 +8,16 @@ import { AuthGuard } from './auth/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', component: WelcomeComponent},
-  { path: 'signup', component: SignupComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard]},
+  { path: '', component: TrainingComponent},
+  // { path: 'signup', component: SignupComponent},
+  // { path: 'login', component: LoginComponent},
+  // { path: 'training', component: TrainingComponent, canActivate: [AuthGuard]},
+  { path: 'training', component: TrainingComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [AuthGuard]
+  // providers: [AuthGuard]
 })
 export class AppRoutingModule { }
